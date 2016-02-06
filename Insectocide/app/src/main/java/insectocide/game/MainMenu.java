c2f21a8a81aeafbd885bc15edf59aba6154e8391
tal2k4xj <tal.neeman4489@gmail.com>
2016-02-06 18:13:54 +0200

    .

--- Insectocide/app/src/main/java/insectocide/game/MainMenu.java
+++ Insectocide/app/src/main/java/insectocide/game/MainMenu.java
@@ -1,13 +1,30 @@
 package insectocide.game;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        View singlePlayerButton = findViewById(R.id.singlePlayerButton);
+        singlePlayerButton.setOnClickListener(this);
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.singlePlayerButton:
+                Intent singlePlayer = new Intent (this,SinglePlayerMenu.class);
+                startActivity(singlePlayer);
+                break;
+        }
+
     }
 }
--- Insectocide/app/src/main/java/insectocide/logic/Player.java
+++ Insectocide/app/src/main/java/insectocide/logic/Player.java
@@ -4,4 +4,5 @@ package insectocide.logic;
  * Created by DELL1 on 2/6/2016.
  */
 public class Player {
+
 }
