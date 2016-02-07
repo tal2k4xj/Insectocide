e7e0ba8c5b806f1f4ccc6770d764740000eeb931
tal2k4xj <tal.neeman4489@gmail.com>
2016-02-07 10:37:49 +0200

    add insect images and insect animation

--- Insectocide/app/src/main/java/insectocide/game/SinglePlayerGame.java
+++ Insectocide/app/src/main/java/insectocide/game/SinglePlayerGame.java
@@ -1,6 +1,7 @@
 package insectocide.game;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -15,6 +16,8 @@ public class SinglePlayerGame extends Activity implements SensorEventListener {
     private Sensor accelerometer;
     private SensorManager sm;
     private ImageView redShip;
+    private ImageView blueInsect;
+    private AnimationDrawable blueInsectAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,10 @@ public class SinglePlayerGame extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_single_player_game);
 
         redShip = (ImageView) findViewById(R.id.redShip);
+        blueInsect = (ImageView) findViewById(R.id.blueInsect);
+        blueInsect.setBackgroundResource(R.drawable.blue_animation);
+        blueInsectAnimation = (AnimationDrawable) blueInsect.getBackground();
+        blueInsectAnimation.start();
 
         y = (TextView) findViewById(R.id.y);
 
@@ -33,12 +40,42 @@ public class SinglePlayerGame extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         float curMovement = event.values[1];
+        int lastMovement = 0 ;
         y.setText("y = " + curMovement);
-        if (curMovement >1) {
+        if (curMovement != lastMovement){
+            if (curMovement > 0.5 && curMovement < 1.5) {
+                redShip.setX(redShip.getX() + 2);
+                redShip.setBackgroundResource(R.drawable.red);
+            }else if(curMovement > 1.5 && curMovement < 3){
+                redShip.setX(redShip.getX() + 4);
+                redShip.setBackgroundResource(R.drawable.redright2);
+            }else if(curMovement > 3){
+                redShip.setX(redShip.getX() + 6);
+                redShip.setBackgroundResource(R.drawable.redright3);
+            }else if(curMovement < -0.5 && curMovement > -1.5){
+                redShip.setX(redShip.getX() - 2);
+                redShip.setBackgroundResource(R.drawable.red);
+            }else if(curMovement < -1.5 && curMovement > -3){
+                redShip.setX(redShip.getX() - 4);
+                redShip.setBackgroundResource(R.drawable.redleft2);
+            }else if(curMovement < -3){
+                redShip.setX(redShip.getX() - 6);
+                redShip.setBackgroundResource(R.drawable.redleft3);
+            }
+        }else if (curMovement > 0.5 && curMovement < 1.5) {
             redShip.setX(redShip.getX() + 2);
-        }else if(curMovement<-1){
+        }else if(curMovement > 1.5 && curMovement < 3){
+            redShip.setX(redShip.getX() + 4);
+        }else if(curMovement > 3){
+            redShip.setX(redShip.getX() + 6);
+        }else if(curMovement < -0.5 && curMovement > -1.5){
             redShip.setX(redShip.getX() - 2);
+        }else if(curMovement < -1.5 && curMovement > -3){
+            redShip.setX(redShip.getX() - 4);
+        }else if(curMovement < -3){
+            redShip.setX(redShip.getX() - 6);
         }
+        lastMovement = (int)curMovement;
     }
 
     @Override
