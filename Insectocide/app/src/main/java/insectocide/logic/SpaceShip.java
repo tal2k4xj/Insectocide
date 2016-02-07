e8a7055017a5f5729cfbd897b41e0b8180ea77c7
Zukis87 <zukis87@gmail.com>
2016-02-07 09:14:23 +0200

    added ship movement

--- Insectocide/app/src/main/java/insectocide/logic/SpaceShip.java
+++ Insectocide/app/src/main/java/insectocide/logic/SpaceShip.java
@@ -1,21 +1,31 @@
 package insectocide.logic;
 
 import android.content.Context;
+import android.view.View;
 
-/**
- * Created by Zukis87 on 06/02/2016.
- */
 public class SpaceShip extends SpaceEntity {
     private final int MAX_ENTITY = 6;
     private final int MIN_ENTITY = 1;
     private final int DEFAULT_HEALTH = 3;
+    private String color;
+    private static String lastMovement;
 
-    public SpaceShip(Context context) {
+    public SpaceShip(String color, Context context) {
         super(context);
         this.firePower = MIN_ENTITY;
         this.fireSpeed = MIN_ENTITY;
         this.health = DEFAULT_HEALTH;
         this.movementSpeed = MIN_ENTITY;
+        this.color = color;
+        lastMovement = "middle";
+        setVisibility(View.VISIBLE);
+        setY(0);
+        setImage(color + lastMovement);
+    }
+
+    private void setImage(String imageName) {
+        int drawableId = getResources().getIdentifier(imageName, "drawable", "insectocide.game");
+        this.setBackgroundResource(drawableId);
     }
 
     @Override
@@ -25,6 +35,23 @@ public class SpaceShip extends SpaceEntity {
 
     @Override
     public void move(String direction) {
-
+        if (!direction.equals(lastMovement)) {
+            setImage(color + direction);
+        }
+        switch (direction) {
+            case "left2":
+                setX(getX()- (movementSpeed*2));
+                break;
+            case "left3":
+                setX(getX()- (movementSpeed*3));
+                break;
+            case "right2":
+                setX(getX()+ (movementSpeed*2));
+                break;
+            case "right3":
+                setX(getX()+ (movementSpeed*3));
+                break;
+        }
+        lastMovement=direction;
     }
 }
