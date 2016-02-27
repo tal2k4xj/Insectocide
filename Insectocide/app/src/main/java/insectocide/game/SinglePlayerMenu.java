878a9a486a47f5485408907933b5bd5c712574f4
tal2k4xj <tal.neeman4489@gmail.com>
2016-02-27 20:07:16 +0200

    buttons fix

--- Insectocide/app/src/main/java/insectocide/game/SinglePlayerMenu.java
+++ Insectocide/app/src/main/java/insectocide/game/SinglePlayerMenu.java
@@ -8,7 +8,6 @@ import android.view.View;
 public class SinglePlayerMenu extends Activity implements View.OnClickListener {
 
     private View newGameButton;
-    private View continueGameButton;
     private View scoreBoardButton;
 
     @Override
@@ -18,8 +17,6 @@ public class SinglePlayerMenu extends Activity implements View.OnClickListener {
 
         newGameButton = findViewById(R.id.newGameButton);
         newGameButton.setOnClickListener(this);
-        continueGameButton = findViewById(R.id.continueGameButton);
-        continueGameButton.setOnClickListener(this);
         scoreBoardButton = findViewById(R.id.scoreBoardButton);
         scoreBoardButton.setOnClickListener(this);
 
@@ -33,11 +30,6 @@ public class SinglePlayerMenu extends Activity implements View.OnClickListener {
                 Intent newGame = new Intent (this,SinglePlayerGame.class);
                 startActivity(newGame);
                 break;
-            case R.id.continueGameButton:
-                continueGameButton.setBackgroundResource(R.drawable.continuebuttonpressed);
-                Intent continueGame = new Intent (this,SinglePlayerGame.class);
-                startActivity(continueGame);
-                break;
             case R.id.scoreBoardButton:
                 scoreBoardButton.setBackgroundResource(R.drawable.scoreboardbuttonpressed);
                 Intent scoreBoard = new Intent (this,ScoreBoardMenu.class);
@@ -50,7 +42,6 @@ public class SinglePlayerMenu extends Activity implements View.OnClickListener {
     protected void onResume() {
         super.onResume();
         newGameButton.setBackgroundResource(R.drawable.newgamebutton);
-        continueGameButton.setBackgroundResource(R.drawable.continuebutton);
         scoreBoardButton.setBackgroundResource(R.drawable.scoreboardbutton);
     }
 }
