6ad2c5f42a900e6b1249977b5753005c2b3cc016
Zukis87 <zukis87@gmail.com>
2016-03-13 20:21:30 +0200

    added provider support for multi

--- Insectocide/app/src/main/java/insectocide/logic/InsectsProvider.java
+++ Insectocide/app/src/main/java/insectocide/logic/InsectsProvider.java
@@ -9,41 +9,49 @@ import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class InsectsProvider {
+    private final String MULTIPLAYER_CONFIGURATION = "0u-0d-0u-0d-0u-0u-0d-0u-0d-0u-0d-2u-4d-3u-1d-1d-3u-4d-2u-0d-0u-2d-4u-3d-1u-1u-3d-4u-2d-0u-0d-0u-0d-0u-0d-0d-0u-0d-0u-0d";
     private Insect[][] insects;
     private CopyOnWriteArrayList<Insect> liveInsects;
     private List<InsectType> insectTypes;
     private int rows,cols;
     private Context context;
     private DisplayMetrics metrics;
-    private String mode;
     private Random rand;
 
     public InsectsProvider(int rows, int cols,String mode, Context context, DisplayMetrics metrics) {
         this.rows = rows;
         this.cols = cols;
-        this.mode = mode;
         this.context = context;
         this.metrics = metrics;
         insects = new Insect[cols][rows];
         liveInsects = new CopyOnWriteArrayList<>();
         insectTypes = new LinkedList<>();
         rand = new Random();
-        generateInsects();
+        if (mode.equals("single")) {
+            generateInsectsForSingle();
+        }else{
+            generateInsectsForMulti();
+        }
     }
 
-    private Insect[][] generateInsects(){
+    private Insect[][] generateInsectsForSingle(){
         CreateInsectTypes();
         for (int i=0 ; i < cols ; i++){
             for(int j=0 ; j < rows ; j++){
-                if(mode.equals("single")) {
-                    insects[i][j] = new Insect(pickRandomTypeFromList(), "down", context, metrics);
-                }else{
-                    if((i%2 == 0 && i%2 == 0) || (i%2 == 1 && j%2 == 1)) {
-                        insects[i][j] = new Insect(pickRandomTypeFromList(), "down", context, metrics);
-                    }else {
-                        insects[i][j] = new Insect(pickRandomTypeFromList(), "up", context, metrics);
-                    }
-                }
+                insects[i][j] = new Insect(pickRandomTypeFromList(), "down", context, metrics);
+                insects[i][j].setPositionAndDimensions(i,j,rows);
+                insects[i][j].bringToFront();
+                liveInsects.add(insects[i][j]);
+            }
+        }
+        return insects;
+    }
+    private Insect[][] generateInsectsForMulti(){
+        String[] configs = MULTIPLAYER_CONFIGURATION.split("-");
+        CreateInsectTypes();
+        for (int i=0 ; i < cols ; i++){
+            for(int j=0 ; j < rows ; j++){
+                insects[i][j] = createNewInsectFromConfiguration(configs[i+j*cols]);
                 insects[i][j].setPositionAndDimensions(i,j,rows);
                 insects[i][j].bringToFront();
                 liveInsects.add(insects[i][j]);
@@ -68,6 +76,12 @@ public class InsectsProvider {
     private InsectType pickRandomTypeFromList(){
         int n = rand.nextInt(insectTypes.size());
         return insectTypes.remove(n);
+    }
+    private Insect createNewInsectFromConfiguration(String configuration){
+        int type = Integer.parseInt(configuration.substring(0,0));
+        char directionChar = configuration.charAt(1);
+        String direction = directionChar=='u' ? "up" : "down";
+        return new Insect(InsectType.values()[type], direction , context, metrics);
     }
     public Insect[][] getInsectMatrix(){
         return insects;
